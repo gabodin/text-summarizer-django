b25ae43f839ec1b47b6d67960ae47fa0b8914a41
gabodin <vgabriel2211@gmail.com>
2024-07-03 11:07:28 -0300

    COnfigurações iniciais

---
+++ summarizer_app/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+# Create your views here.
+token_name = 'unicamp-dl/ptt5-base-portuguese-vocab'
+model_name = 'recogna-nlp/ptt5-base-summ'
+
+tokenizer = T5Tokenizer.from_pretrained(token_name)
+model_pt = T5ForConditionalGeneration.from_pretrained(model_name)
+
+
+def summarizer(request):
+    if request.method == 'POST':
+        # Get the user input
+        text = request.POST['text']
+    
+        inputs = tokenizer.encode(text,
+                                  max_length=1024,
+                                  truncation=True,
+                                  return_tensors='pt'
+                                  )
+        summary_ids = model_pt.generate(inputs, max_length=256, min_length=128, num_beams=5, no_repeat_ngram_size=3, early_stopping=True)
+        summary = tokenizer.decode(summary_ids[0])
+
+        # Render the template with the prediction result
+        return render(request,
+                      'summarizer_app/text-summarizer.html',
+                      {'summary': summary})
+     
+    return render(request, 'summarizer_app/text-summarizer.html', {'summary': ""})
