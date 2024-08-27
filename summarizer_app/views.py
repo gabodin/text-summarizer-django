1a6d8aef78eee3dd10fc87251be0e893d4a9f3ae
gabodin <vgabriel2211@gmail.com>
2024-08-27 11:27:50 -0300

    feat: user defined text minimum size

--- summarizer_app/views.py
+++ summarizer_app/views.py
@@ -12,41 +12,28 @@ tokenizer = T5Tokenizer.from_pretrained(token_name)
 model_pt = T5ForConditionalGeneration.from_pretrained(model_name)
 
 
-def summarizer(request):
-    if request.method == 'POST':
-        # Get the user input
-        text = request.POST['text']
-    
-        inputs = tokenizer.encode(text,
-                                  max_length=1024,
-                                  truncation=True,
-                                  return_tensors='pt'
-                                  )
-        summary_ids = model_pt.generate(inputs, max_length=256, min_length=128, num_beams=5, no_repeat_ngram_size=3, early_stopping=True)
-        summary = tokenizer.decode(summary_ids[0])
-
-        # Render the template with the prediction result
-        return render(request,
-                      'summarizer_app/text-summarizer.html',
-                      {'summary': summary})
-     
+def summarizer(request):   
     return render(request, 'summarizer_app/text-summarizer.html', {'summary': ""})
 
 
 def summary_result(request):
     if request.method == 'POST':
         text = request.POST.get('text', '')
-        # print(text)
+        min_size = int(request.POST.get('result-size'))
         
         inputs = tokenizer.encode(text,
                                   max_length=4096,
                                   truncation=True,
                                   return_tensors='pt'
                                   )
-        summary_ids = model_pt.generate(inputs, max_length=512, min_length=128, num_beams=5, no_repeat_ngram_size=3, early_stopping=True)
+     
+        summary_ids = model_pt.generate(inputs, max_length=640,
+                                        min_length=min_size, num_beams=5,
+                                        no_repeat_ngram_size=3, 
+                                        early_stopping=True
+                                        )
+       
         summary = tokenizer.decode(summary_ids[0])
-
-        # Render the template with the prediction result
         
         response_data = {
             'message': 'Success',
